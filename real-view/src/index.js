67424f46d06f4536d6fb233dfad73040b29e87eb
Preston <prwillis2@gmail.com>
2024-11-03 18:50:38 -0500

    f

--- real-view/src/index.js
+++ real-view/src/index.js
@@ -17,7 +17,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Layout />} />
-          <Route path="home" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="listings" element={<Listings />} />
           <Route path="about" element={<About />} />
           <Route path="agents" element={<Agents />} />
